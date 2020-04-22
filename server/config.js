399e4ef5a84616f12420d72ac2c5d7d121be2a0d
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2020-04-21 23:10:43 -0400

    final update

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	MONGODB: 'mongodb+srv://kaixuan:happycoding@courseapi-ujwvo.mongodb.net/nyu?retryWrites=true&w=majority',
-	CURRENT_TERM: "Fall 2019",
-	SECRET_KEY: "This is secret"
-}
